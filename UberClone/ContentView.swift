9817fc7b0e531886e6b9e3a2a7869d086c2718ea
TalaSadeq <talajamalsadeq@gmail.com>
2023-10-13 11:05:48 -0700

    Initial Commit

---
+++ UberClone/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UberClone
+//
+//  Created by Tala Sadeq on 10/13/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UberClone/UberCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberCloneApp.swift
+//  UberClone
+//
+//  Created by Tala Sadeq on 10/13/23.
+//
+
+import SwiftUI
+
+@main
+struct UberCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
